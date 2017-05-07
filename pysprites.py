0e47f110ec95e7cdd7738e9eedad2d243060a654
João Gonçalves <jsvgoncalves@gmail.com>
2017-05-07 09:36:07 +0200

    Refactor and comment code
    * Minor refactorings and code aesthetics improvements.
    
    Signed-off-by: Joao Goncalves <jsvgoncalves@gmail.com>

--- pysprites.py
+++ pysprites.py
@@ -28,27 +28,43 @@ def get_from_folder(name, ext='.png'):
     return filelist
 
 
-def main(images, output=None, cols=None):
-    if path.isdir(images[0]):
-        images = get_from_folder(images[0])
+def main(sources, output='spritesheet.png', cols=None):
+    """PySprites entry point.
 
+    Args:
+        sources (str): A list of sources or a folder
+        output (str, optional): Destination file
+        cols (int, optional): Number of columns of the spritesheet
+    """
+
+    # Arguments or defaults
+    # Note: The arguments can't be set as default, as None is passed
+    #       by ArgumentParser().
+    if cols is None:
+        cols = 2  # cols, rows
+    if output is None:
+        output = 'spritesheet.png'
+
+    # If `sources` is a folder, get the contents
+    if path.isdir(sources[0]):
+        sources = get_from_folder(sources[0])
+
+    # Open all the images with PIL
     try:
-        im = [Image.open(i).convert('RGBA') for i in images]
+        im = [Image.open(i).convert('RGBA') for i in sources]
     except IOError as ioe:
         print('Error: {}'.format(ioe))
         exit(1)
 
-    # Pre-processing
-    if cols is None:
-        cols = 2  # cols, rows
-    dims = (cols, int(ceil(len(im) / cols)))
-
+    # Pre-computer sizes and dimensions
+    dims = (cols, int(ceil(len(im) / cols)))  # rows = total_images / columns
+    # !TODO: Add option to have sprites of multiple sizes
     sprite_size = im[0].size  # width, height
+    # spritesheet_size = (cols, rows) x (sprite_width, sprite_height)
     spritesheet_size = ([a * b for a, b in zip(sprite_size, dims)])
 
+    # Generate final Image and paste in the sprites
     final = Image.new('RGBA', spritesheet_size, (0, 0, 0, 123))
-
-    # Paste all the images
     try:
         for row in xrange(0, dims[1]):
             for col in xrange(0, dims[0]):
@@ -56,14 +72,12 @@ def main(images, output=None, cols=None):
                             (col * sprite_size[0], row * sprite_size[1]))
 
     # !TODO: Fix this when `len(im)%cols > 0`
+    # Note: This always happens when the final spritesheet will contain empty
+    #       columns at the end.
     except IndexError as ie:
         print('Error: {}'.format(ie))
         print('Continuing... The rest of the image will be empty')
 
-    # Display image
-    # final.show()
-    if output is None:
-        output = 'spritesheet.png'
     final.save(output)
 
     print("# Created Spritesheet '{}' ({} x {} px)".format(
@@ -74,12 +88,13 @@ if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'image',
+        'sources',
         help='images or a folder that contains the images', type=str,
         nargs='+')
     parser.add_argument(
         '-o', '--output',
-        help='output to given file')
+        help='output to given file\
+        \nDEFAULT: spritesheet.png')
     parser.add_argument(
         '-c', '--columns',
         help='number of columns per row, \
@@ -88,4 +103,4 @@ if __name__ == '__main__':
         type=int)
     args = parser.parse_args()
 
-    main(images=args.image, output=args.output, cols=args.columns)
+    main(sources=args.sources, output=args.output, cols=args.columns)
